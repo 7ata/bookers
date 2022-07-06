c59ba573d1534e7aeb68acaef6e104238e7d7d53
7ata <103552711+7ata@users.noreply.github.com>
2022-07-06 12:33:40 +0000

    3-11[Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,37 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all  
     @book = Book.new
+  end
+  
+  def show
+    @book = Book.find(params[:id])  
   end
 
 # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/edit'
-  end
-
-  def show
+    @book.save
+  		redirect_to book_path(@book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
   end
 
   def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path # 投稿一覧画面へリダイレクト  
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books
-
+  get '/books' => 'books#index'
+  # .../lists/1 や .../lists/3 に該当する
+  get '/books/:id' => 'books#show' 
+  get '/books/:id/edit' => 'books#edit' 
+  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
